3c5aca824997897c03162a8ef3886f5fe7705014
Scott Peterson <scottpeterson07@gmail.com>
2016-10-13 19:57:33 -0500

    implemented a slider input to draw a Mario-style pyramid automatically.

---
+++ pyramid-slide.js
@@ -0,0 +1,40 @@
+
+
+
+var height = document.getElementById("range")
+
+//This code is taken from the Mario 4 studio, since it is not necessary to rewrite the draw function.
+
+/**
+ * drawPyramid
+ *
+ * Renders, in the HTML document, a Mario-style pyramid of the height specified by the slider.
+ */
+function drawPyramid(height) {
+
+    // first, clear the old content
+    document.getElementById("pyramid").innerHTML = "";
+
+    // for each row....
+    for (var row = 0; row < height; row++) {
+
+        // figure out number of bricks and spaces
+        var numBricks = row + 2;
+        var numSpaces = height - row - 1;
+
+        // build up a string for this row
+        var rowStr = "";
+        for (var i = 0; i < numSpaces; i++) {
+            var spaceChar = "&nbsp"; // this is the HTML encoding for a space " "
+            rowStr += spaceChar;
+        }
+        for (var i = 0; i < numBricks; i++) {
+            rowStr += "#";
+        }
+
+        // make a <p> element for this row, and insert it into the #pyramid container
+        rowElem = document.createElement("p");
+        rowElem.innerHTML = rowStr;
+        document.getElementById("pyramid").appendChild(rowElem);
+    }
+}
